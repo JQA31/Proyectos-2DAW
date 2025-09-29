5aa56f237bd7583347344d115175392be1aa4591
JQA31 <josephjoelquispealvarez.guadalupe@alumnado.fundacionloyola.net>
2025-09-29 14:20:58 +0200

    Add files via upload

---
+++ CalentamientoGlobal/funciones.php
@@ -0,0 +1,20 @@
+<?php
+    function paises(){
+        return $paises = [
+            ["nombre" => "Perú", "codigo" => "peru"],
+            ["nombre" => "España", "codigo" => "espana"],
+            ["nombre" => "Chile", "codigo" => "chile"],
+            ["nombre" => "Argentina", "codigo" => "argentina"],
+            ["nombre" => "Colombia", "codigo" => "colombia"]
+        ];
+    }
+    
+    function intereses(){
+       return $intereses = [
+            ["nombre" => "Medioambiente", "codigo" => "medioambiente"],
+            ["nombre" => "Tecnologia", "codigo" => "tecnologia"],
+            ["nombre" => "Ciencia", "codigo" => "ciencia"]
+        ];
+    }
+    
+?>
