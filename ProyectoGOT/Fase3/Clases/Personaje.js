7ffaf495a20409260bd0d1373c4e1aa3c7e5a132
JQA31 <josephjoelquispealvarez.guadalupe@alumnado.fundacionloyola.net>
2025-11-08 20:55:42 +0100

    Add files via upload

---
+++ ProyectoGOT/Fase3/Clases/Personaje.js
@@ -0,0 +1,68 @@
+import Arma from "./Arma.js";
+
+class Personaje{
+    #nombre;
+    #edad;
+    #vivo;
+    #casa;
+    #frase;
+
+    constructor(nombre,edad,casa,vivo){
+        this.#nombre = nombre;
+        this.#edad = edad;
+        this.#vivo = vivo;
+        this.#casa = casa;
+        this.#frase = `Soy ${this.#nombre} de la ${this.#casa}`;
+    }
+
+    //Geters and Setters
+    getNombre() {
+        return this.#nombre;
+    }
+
+    setNombre(nombre) {
+        this.#nombre = nombre;
+    }
+
+    getEdad() {
+        return this.#edad;
+    }
+
+    setEdad(edad) {
+        this.#edad = edad;
+    }
+
+    getCasa() {
+        return this.#casa;
+    }
+
+    setCasa(casa) {
+        this.#casa = casa;
+    }
+
+    decirFrase() {
+        console.log(this.#frase);
+    }
+
+    estado(){
+        if(this.#vivo){
+            console.log("Esta vivo");
+        }else{
+            console.log("Esta muerto");
+        }
+    }
+
+    getEstado(){
+        return this.#vivo;
+    }
+
+    //Metodos
+
+    //Cambia el estado de vivo a muerto
+    morir(){
+        console.log(`${this.#nombre} ha muerto.`);
+        this.#vivo = false;
+    }
+}
+
+export default Personaje;
